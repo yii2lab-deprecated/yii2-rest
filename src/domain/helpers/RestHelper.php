12c9f9e34dc14173065e0e92df3d2d9f83a390f6
Vitaliy <theyamshikov@yandex.ru>
2018-02-27 17:46:41 +0600

    refacto

--- src/domain/helpers/RestHelper.php
+++ src/domain/helpers/RestHelper.php
@@ -5,8 +5,8 @@ namespace yii2lab\rest\domain\helpers;
 use yii\web\ServerErrorHttpException;
 use yii\httpclient\Client;
 use yii2lab\rest\domain\entities\RequestEntity;
-use yii\httpclient\Request;
 use yii2lab\misc\enums\HttpMethodEnum;
+use yii2lab\rest\domain\entities\ResponseEntity;
 
 class RestHelper {
 
@@ -30,33 +30,29 @@ class RestHelper {
         return self::runRequest(compact('uri', 'data', 'headers', 'options', 'method'));
     }
 
+    /**
+     * @param RequestEntity $requestEntity
+     * @throws
+     *
+     * @return ResponseEntity
+     */
     public static function sendRequest(RequestEntity $requestEntity) {
-        return self::runRequest($requestEntity);
-    }
-
-    protected static function runRequest($data) {
-        if($data instanceof RequestEntity) {
-            $requestEntity = $data;
-        } else {
-            $requestEntity = new RequestEntity;
-            $requestEntity->load($data);
-        }
-        $request = self::createHttpRequest($requestEntity);
+        $request = self::buildRequestClass($requestEntity);
         try {
             $response = $request->send();
         } catch(\yii\httpclient\Exception $e) {
             throw new ServerErrorHttpException('Url "' . $request->url . '" is not available');
         }
-        return $response;
+        return self::buildResponseEntity($response);
     }
 
-    /**
-     * @param RequestEntity $requestEntity
-     * @throws
-     *
-     * @return Request
-     */
-    protected static function createHttpRequest(RequestEntity $requestEntity) {
+    private static function runRequest($data) {
+        $requestEntity = new RequestEntity;
+        $requestEntity->load($data);
+        return self::sendRequest($requestEntity);
+    }
+
+    private static function buildRequestClass(RequestEntity $requestEntity) {
         $requestEntity->validate();
         $httpClient = new Client();
         $request = $httpClient->createRequest();
@@ -68,6 +64,17 @@ class RestHelper {
             ->setHeaders($requestEntity->headers)
             ->addHeaders(['user-agent' => 'Awesome-Octocat-App']);
         return $request;
+    }
+
+    private static function buildResponseEntity($response) {
+        $responseEntity = new ResponseEntity;
+        $responseEntity->data = $response->data;
+        $responseEntity->headers = $response->headers;
+        $responseEntity->content = $response->content;
+        $responseEntity->format = $response->format;
+        $responseEntity->cookies = $response->cookies;
+        $responseEntity->status_code = $response->statusCode;
+        return $responseEntity;
     }
 
 }
