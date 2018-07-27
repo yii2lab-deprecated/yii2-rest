b96cd72694c17681335448ac4c297bf0adc27cc6
Vitaliy <theyamshikov@yandex.ru>
2018-07-27 22:54:40 +0600

    refacto search

--- src/domain/rest/SearchAction.php
+++ src/domain/rest/SearchAction.php
@@ -4,6 +4,7 @@ namespace yii2lab\rest\domain\rest;
 
 use Yii;
 use yii2lab\domain\services\base\BaseActiveService;
+use yii2lab\extension\activeRecord\helpers\SearchHelper;
 use yii2lab\helpers\ClientHelper;
 
 /**
@@ -19,7 +20,7 @@ class SearchAction extends IndexAction {
 		$getParams = Yii::$app->request->get();
 		$query = ClientHelper::getQueryFromRequest($getParams);
 		$text = Yii::$app->request->post('title');
-		$query->where(BaseActiveService::SEARCH_PARAM_NAME, $text);
+		$query->where(SearchHelper::SEARCH_PARAM_NAME, $text);
 		return $this->service->getDataProvider($query);
 	}
 
