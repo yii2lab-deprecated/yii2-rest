f282ac1a3dc9522fb416903f82830d21a2d8fc20
Vitaliy <theyamshikov@yandex.ru>
2018-07-27 17:06:09 +0600

    upgrade search action

--- src/domain/rest/SearchAction.php
+++ src/domain/rest/SearchAction.php
@@ -3,6 +3,7 @@
 namespace yii2lab\rest\domain\rest;
 
 use Yii;
+use yii2lab\domain\data\Query;
 use yii2lab\domain\services\base\BaseActiveService;
 use yii2lab\helpers\ClientHelper;
 
@@ -16,13 +17,20 @@ class SearchAction extends IndexAction {
 	public function run() {
 		$getParams = Yii::$app->request->get();
 		$query = ClientHelper::getQueryFromRequest($getParams);
-		foreach($this->fields as $key) {
-			$param = Yii::$app->request->post($key);
-			if(!empty($param)) {
-				$query->andWhere(['ilike', $key, $param]);
-			}
+		$title = Yii::$app->request->post('title');
+		if(empty($title) || mb_strlen($title) < 3) {
+			return [];
 		}
+		$likeCondition = $this->generateLikeCondition($title);
+		$query->andWhere($likeCondition);
 		return $this->service->getDataProvider($query);
 	}
 
+	private function generateLikeCondition($title) {
+		$q = Query::forge();
+		foreach($this->fields as $key) {
+			$q->orWhere(['ilike', $key, $title]);
+		}
+		return $q->getParam('where');
+	}
 }
