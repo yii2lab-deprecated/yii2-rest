34aa8da93512e2eae53f98b5803db1f23f26951b
Vitaliy <theyamshikov@yandex.ru>
2018-07-28 00:55:44 +0600

    refacto search

--- src/domain/rest/SearchAction.php
+++ src/domain/rest/SearchAction.php
@@ -3,6 +3,9 @@
 namespace yii2lab\rest\domain\rest;
 
 use Yii;
+use yii\web\BadRequestHttpException;
+use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
+use yii2lab\domain\helpers\ErrorCollection;
 use yii2lab\domain\services\base\BaseActiveService;
 use yii2lab\extension\activeRecord\helpers\SearchHelper;
 use yii2lab\helpers\ClientHelper;
@@ -21,7 +24,13 @@ class SearchAction extends IndexAction {
 		$query = ClientHelper::getQueryFromRequest($getParams);
 		$text = Yii::$app->request->post('title');
 		$query->where(SearchHelper::SEARCH_PARAM_NAME, $text);
-		return $this->service->getDataProvider($query);
+		try {
+			return $this->service->getDataProvider($query);
+		} catch(BadRequestHttpException $e) {
+			$error = new ErrorCollection;
+			$error->add('title', $e->getMessage());
+			throw new UnprocessableEntityHttpException($error);
+		}
 	}
 
 }
