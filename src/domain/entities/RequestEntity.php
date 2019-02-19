eabdbfbff381e5426ed4d90561d1ccfcf170488e
amubarak <amubarak@wooppay.com>
2019-02-19 17:18:23 +0600

    add new functional bt header content type
    (cherry picked from commit a2d8412b6c5bc4b1b360f9ca02ad0d575c8ac400)

--- src/domain/entities/RequestEntity.php
+++ src/domain/entities/RequestEntity.php
@@ -2,8 +2,11 @@
 
 namespace yii2lab\rest\domain\entities;
 
+use yii\helpers\Json;
 use yii2lab\domain\BaseEntity;
-use yii2lab\misc\enums\HttpMethodEnum;
+use yii2lab\extension\web\enums\HttpMethodEnum;
+use yii2lab\rest\domain\enums\ContentTriggerHeadersEnum;
+use yii2mod\helpers\ArrayHelper;
 
 /**
  * Class RequestEntity
@@ -14,9 +17,16 @@ use yii2lab\misc\enums\HttpMethodEnum;
  * @property $data array
  * @property $headers array
  * @property $options array
+ * @property $content array
  * @property $cookies array
+ * @property $format string
+ * @property-read $post array
+ * @property-read $query array
+ * @property $description string
+ * @property $authorization string
  */
-class RequestEntity extends BaseEntity {
+class RequestEntity extends BaseEntity
+{
 
 	protected $method = HttpMethodEnum::GET;
 	protected $uri;
@@ -24,12 +34,51 @@ class RequestEntity extends BaseEntity {
 	protected $headers = [];
 	protected $options = [];
 	protected $cookies = [];
-	
-	public function rules() {
+	protected $content = null;
+	protected $format = null;
+	protected $description = null;
+	protected $authorization = null;
+
+	public function rules()
+	{
 		return [
 			[['uri'], 'required'],
 			[['method'], 'in', 'range' => HttpMethodEnum::values()],
 		];
 	}
 
+	public function getMethod()
+	{
+		return strtoupper($this->method);
+	}
+
+	public function getPost()
+	{
+		return $this->data;
+	}
+
+	public function getQuery()
+	{
+		return $this->data;
+	}
+
+	public function setHeaders($value)
+	{
+		if ($value) {
+			$headers = [];
+			foreach ($value as $key => $value) {
+				$key = mb_strtolower($key);
+				if (is_array($value)) {
+					$headers[$key] = ArrayHelper::first($value);
+				} else {
+					$headers[$key] = $value;
+				}
+				if (!empty(ContentTriggerHeadersEnum::value($key))) {
+					$this->content = Json::encode($this->data, 0);
+				}
+			}
+
+			$this->headers = $headers;
+		}
+	}
 }
