adc00102bb65649d51971c564b911e4615e3e061
amubarak <amubarak@wooppay.com>
2019-09-12 15:54:15 +0600

    Revert "Added search with partner-name in service/search"
    This reverts commit d66e8c9f

--- src/domain/rest/SearchAction.php
+++ src/domain/rest/SearchAction.php
@@ -2,7 +2,6 @@
 
 namespace yii2lab\rest\domain\rest;
 
-
 use Yii;
 use yii\web\BadRequestHttpException;
 use yii2lab\domain\exceptions\UnprocessableEntityHttpException;
@@ -10,7 +9,6 @@ use yii2lab\domain\helpers\ErrorCollection;
 use yii2lab\domain\services\base\BaseActiveService;
 use yii2lab\extension\activeRecord\helpers\SearchHelper;
 use yii2lab\extension\web\helpers\ClientHelper;
-
 
 /**
  * @property BaseActiveService $service
@@ -27,9 +25,6 @@ class SearchAction extends IndexAction {
 		$text = Yii::$app->request->post('title');
 		$query->where(SearchHelper::SEARCH_PARAM_NAME, $text);
 		try {
-            if ( Yii::$app->request->headers->get('partner-name') ){
-                return \App::$domain->service->service->getSearchByPartnerName($query);
-            }
 			return $this->service->getDataProvider($query);
 		} catch(BadRequestHttpException $e) {
 			$error = new ErrorCollection;
